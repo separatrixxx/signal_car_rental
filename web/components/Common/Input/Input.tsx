4833e9670993a1c788d08a5b993cdbaa5240c585
separatrixxx <nikitalochmatov@gmail.com>
2024-04-26 01:42:41 +0300

    update

--- web/components/Common/Input/Input.tsx
+++ web/components/Common/Input/Input.tsx
@@ -4,12 +4,19 @@ import cn from 'classnames';
 
 
 export const Input = ({ type, text, value, minDate, error, isSearch, onChange }: InputProps): JSX.Element => {
+    const now = new Date();
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Месяцы начинаются с 0
+    const year = now.getFullYear();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    
 	return <input className={cn(styles.input, {
         [styles.error_input]: error,
         [styles.search]: isSearch,
     })}
         placeholder={text}
-        value={value}
+        value={value.length === 0 ? `${year}-${month}-${day}T${hours}:${minutes}` : value}
         onChange={onChange}
         type={type === 'text' ? 'text' : type === 'date' ? 'datetime-local' : 'phone'}
         name={type}
