4146a21e3d96413b2b39eb6ec020ba8b552e1d0d
separatrixxx <nikitalochmatov@gmail.com>
2024-07-08 12:45:31 +0300

    update

--- web/components/Common/LoadingDots/LoadingDots.tsx
+++ web/components/Common/LoadingDots/LoadingDots.tsx
@@ -1,7 +1,7 @@
 import styles from './LoadingDots.module.css';
 
+
 export const LoadingDots = (): JSX.Element => {
-  
     return (
         <span className={styles.loading}>
             <span />
