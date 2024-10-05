6ca65c9d56b63547f41b0e803ffae809093fc4ec
separatrixxx <nikitalochmatov@gmail.com>
2024-10-05 22:25:05 +0300

    fix

--- web/components/Common/ByBlock/ByBlock.props.ts
+++ web/components/Common/ByBlock/ByBlock.props.ts
@@ -1,5 +1,4 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
-import { BookingInterface } from '../../../interfaces/booking.interface';
 
 
 export interface ByBlockProps extends DetailedHTMLProps<HTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
